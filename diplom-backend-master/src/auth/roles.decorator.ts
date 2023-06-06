2ad766148a769fb71add6a088b613210faffd15b
CursedTendu <131683590+CursedTendu@users.noreply.github.com>
2023-06-06 22:38:39 +0700

    Delete diplom-backend-master directory

--- diplom-backend-master/src/auth/roles.decorator.ts
+++
@@ -1,6 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-import { UserRolesEnum } from '../common/types';
-
-export const ROLES_KEY = 'roles';
-export const Roles = (...roles: UserRolesEnum[]) =>
-  SetMetadata(ROLES_KEY, roles);
--- diplom-backend-master/src/common/types/attestation.type.ts
+++
@@ -1,1 +0,0 @@
-export type AttestationScoreType = 0 | 1 | 2;
